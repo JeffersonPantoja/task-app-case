08341a8cc0534fcb91a7f2d383271668aa603800
Jefferson A P Silva <jeffkd35@gmail.com>
2026-06-13 14:15:25 -0300

    feature - Etapa 4: Análise Estática de Código (SAST)

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -5,8 +8,9 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)

e238f2d53c7711496f1db3becee5e7cd88fbd5f7
Jefferson A P Silva <jeffkd35@gmail.com>
2026-06-14 17:55:57 -0300

    Correção de vulnerabilidades

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -11,13 +11,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+app.config['SESSION_COOKIE_HTTPONLY'] = True
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
 bcrypt = Bcrypt(app)
+
+
+@app.after_request
+def add_security_headers(response):
+    response.headers['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:"
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers.pop('Server', None)
+    return response
 
 from todo_project import models
 
